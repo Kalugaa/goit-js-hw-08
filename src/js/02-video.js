d043270895257a55ba1bbdf393b3c5703448c3c9
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-10-09 13:38:20 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,12 +5,10 @@ const vimeoPlayer = new Vimeo('vimeo-player');
 
 const VIDEO_KEY = 'videoplayer-current-time';
 
-// Функція для збереження поточного часу відтворення у локальному сховищі
 function saveCurrentTime(time) {
   localStorage.setItem(VIDEO_KEY, JSON.stringify(time));
 }
 
-// Функція для завантаження поточного часу відтворення з локального сховища
 function loadCurrentTime() {
   const savedTime = localStorage.getItem(VIDEO_KEY);
   return savedTime ? JSON.parse(savedTime) : 0;
