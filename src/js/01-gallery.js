61de4ba819595a5d3e403ac5fc10ee4f1b114f85
Kalugaa <133102595+Kalugaa@users.noreply.github.com>
2023-10-09 12:44:00 +0200

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
+  `;
+}
+
+const galleryItemsMarkup = galleryItems.map(createGalleryItem).join('');
+gallery.innerHTML = galleryItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
---
+++ src/js/02-video.js
@@ -0,0 +1,6 @@
+// const iframe = document.querySelector('#vimeo-player');
+// const player = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
